b1f2f8adc8f6d103488b02bd5100e797a51216a6
Ujjwal-01 <ucujju@gmail.com>
2020-10-24 23:22:46 +0100

    hey there! My first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string guessed_word = "Inception";
+            string guessing_word = "";
+            int attempt = 0;
+            int guessing_word_limit = 3;
+            bool No_more_attempt_left = false;
+
+
+
+            while (guessing_word != guessed_word && !No_more_attempt_left)
+            {
+                if (attempt < guessing_word_limit)
+                {
+                    Console.Write("Enter your guessing word: ");
+                    guessing_word = Console.ReadLine();
+                    attempt++;
+
+                }
+                else No_more_attempt_left = false;
+                {
+                    Console.WriteLine(" Opps!!no more attempts left ");
+
+                }
+                Console.WriteLine("Do you want to try again: Yes/No ? ");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                    break;
+                if (answer == "no")
+                    return;
+
+
+
+
+            }
+        }
+    }
+}
---
+++ adding/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string guessed_word = "Inception";
+            string guessing_word = "";
+            int start_count = 0;
+            int attempt_limit = 3;
+            bool No_more_guesses = false;
+           
+
+            while (guessed_word != guessing_word && !No_more_guesses )
+            {
+
+                if (start_count < attempt_limit)
+                {
+                    textBox1.Text = "Enter your guessing word: ";
+                    start_count++;
+                }
+                else
+                {
+                    No_more_guesses = true;
+                    textBox1.Text = "Sorry! Try again: ";
+                }             
+
+            }
+            if (No_more_guesses)
+            {
+                textBox1.Text = "You lose!!";
+            }
+            else
+            {
+                textBox1.Text = "You have Won";
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "WELCOME TO THE GUESSING GAME!!";
+            label1.Text = "Please check the hints for the word: ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Film was released in 2013: ");
+            MessageBox.Show("Actor: Leonardo Docaprio: ");
+            MessageBox.Show("Director: Christopher Nolan: ");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Please enter your guessing word:";
+            
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Please enter another guess: ";
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Opps!! Last chance: ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click on submit, after finishing");
+        } 
+    }
+}
